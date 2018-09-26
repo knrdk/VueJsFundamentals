bc43072a626bd973b46ef91ebb2109a03868fb0a
knrdk <konrad1991@gmail.com>
2018-09-26 10:41:54 +0200

    chapter7

---
+++ build-a-bot/src/shared/pin-directive.js
@@ -0,0 +1,23 @@
+export default function (element, binding) {
+  Object.keys(binding.value).forEach((position) => {
+    element.style[position] = binding.value[position];
+  });
+
+  element.style.position = 'absolute';
+}
+
+// function applyStyle(element, binding) {
+//   Object.keys(binding.value).forEach((position) => {
+//     element.style[position] = binding.value[position];
+//   });
+
+//   element.style.position = 'absolute';
+// }
+// export default {
+//   bind: (element, binding) => {
+//     applyStyle(element, binding);
+//   },
+//   update: (element, binding) => {
+//     applyStyle(element, binding);
+//   },
+// };
